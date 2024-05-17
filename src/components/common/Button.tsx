2a3d9354521dfb2e4cae9f3f5dc3b9fa44419c41
lsj0202 <w1004123@gmail.com>
2024-05-17 09:23:46 +0900

    feat: add button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,5 +1,106 @@
-const Button = () => {
-  return <div></div>;
+import { cva } from 'class-variance-authority';
+
+import { cn } from '@/utils/style';
+
+type ButtonProps = {
+  /**
+   * 버튼 크기를 지정합니다, 기본값 (md)
+   */
+  size?: 'xs' | 'sm' | 'md';
+  /**
+   * 버튼 색을 지정합니다, 기본값 (black)
+   */
+  color?: 'black' | 'gray' | 'orange' | 'red';
+  /**
+   * 버튼 내부 색상이 칠해져 있는지 여부를 지정합니다
+   */
+  outline?: boolean;
+  /**
+   * 사용자 인터렉션이 진행중인지 여부를 정합니다.
+   */
+  isLoading?: boolean;
+  /**
+   * 버튼의 넓이가 100%인 경우 사용합니다.
+   */
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+} & React.ComponentPropsWithoutRef<'button'>;
+
+const ButtonVariants = cva('relative disabled:opacity-50', {
+  variants: {
+    size: {
+      xs: 'px-2 text-xs',
+      sm: 'px-3 py-1 text-sm',
+      md: 'px-5 py-2 text-base',
+    },
+    fullWidth: {
+      true: 'w-full',
+    },
+    outline: {
+      true: 'text-black',
+      false: 'text-white',
+    },
+    color: {
+      black: '',
+      gray: '',
+      orange: '',
+      red: '',
+    },
+  },
+  compoundVariants: [
+    {
+      outline: true,
+      color: 'black',
+      className: 'border border-black text-black',
+    },
+    {
+      outline: true,
+      color: 'gray',
+      className: 'border border-slate-300 text-black',
+    },
+    {
+      outline: true,
+      color: 'orange',
+      className: 'border border-orange-500 text-black',
+    },
+    {
+      outline: true,
+      color: 'red',
+      className: 'border border-red-700 text-black',
+    },
+    { outline: false, color: 'black', className: 'bg-black' },
+    { outline: false, color: 'gray', className: 'bg-slate-300' },
+    { outline: false, color: 'orange', className: 'bg-orange-500' },
+    { outline: false, color: 'red', className: 'bg-red-500' },
+  ],
+  defaultVariants: {
+    size: 'md',
+    color: 'black',
+    outline: false,
+  },
+});
+
+const Button = ({
+  size = 'md',
+  color = 'black',
+  outline = false,
+  isLoading,
+  fullWidth,
+  children,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      disabled={isLoading || props.disabled}
+      {...props}
+      className={cn(
+        ButtonVariants({ size, fullWidth, outline, color }),
+        props.className,
+      )}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
