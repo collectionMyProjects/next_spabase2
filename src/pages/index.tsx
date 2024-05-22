6fa4c9d53b6b692c05e17e8b053188687f426575
lsj0202 <w1004123@gmail.com>
2024-05-22 22:03:21 +0900

    feat: add faker-js 사용해서 mock data 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from 'react';
+
 import ThunderLayout from '@/components/layout/ThunderLayout';
+import { getProduct } from '@/repository/getProduct';
+import { Product } from '@/types';
 
 const Home = () => {
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    getProduct('id').then((res) => setProduct(res.data));
+  }, []);
+
   return (
     <ThunderLayout>
-      <div className="bg-slate-50" />
+      <div>Sample Product</div>
+      {product && <div>{JSON.stringify(product)}</div>}
     </ThunderLayout>
   );
 };
-
 export default Home;
